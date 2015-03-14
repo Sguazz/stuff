f8bb2eef41ede9615101ddfa9d0126b4f9d2c157
Sguazz <sguazz@Paolos-MacBook-Pro.local>
2015-03-14 19:57:24 +0200

    Computing grades
    The starting point of the modes are now computed instead of being hard
    coded. This is mostly a show off, as it’s arguably less clear.
    
    Also changed some function names.

--- Scales/Scales.hs
+++ Scales/Scales.hs
@@ -2,7 +2,7 @@ module Scales where
 
 import Data.List (intercalate)
 
-data Note = C | Db | D | Eb | E | F | Gb | G | Ab | A | Bb | B
+data Note = C | Cs | D | Ds | E | F | Fs | G | Gs | A | As | B
     deriving (Eq, Ord, Show, Enum)
 
 data Interval = Root | MinorSecond | Second | MinorThird | Third |
@@ -11,7 +11,7 @@ data Interval = Root | MinorSecond | Second | MinorThird | Third |
     deriving (Eq, Ord, Show, Enum)
 
 data Mode = Ionian | Dorian | Phrygian | Lydian | Mixolydian | Aeolian | Locrian
-    deriving (Show)
+    deriving (Eq, Show, Enum)
 
 type Key = Note
 
@@ -19,31 +19,27 @@ notes      = cycle [C ..]
 intervals  = cycle [Root ..]
 majorScale = [Root, Second, Third, PerfectFourth, PerfectFifth, Sixth, Seventh]
 
-grade Ionian     =  0  -- I    (Major)
-grade Dorian     =  2  -- II
-grade Phrygian   =  4  -- III
-grade Lydian     =  5  -- IV
-grade Mixolydian =  7  -- V
-grade Aeolian    =  9  -- VI   (Minor)
-grade Locrian    = 11  -- VII
-
 guitarStrings = [E, B, G, D, A, E]
 neckDots      = [3, 5, 7, 9]
 neckLength    = 13
 
 marks = map (`elem` majorScale) intervals
 
+grade :: Mode -> Int
+grade m = val m . zip [Ionian ..] . map snd . filter fst $ zip marks [0..]
+    where   val m = snd . head . filter ((==m) . fst)
+
 chromatic :: Key -> [Note]
 chromatic k = dropWhile (/=k) notes
 
-modeScale :: Mode -> [Bool]
-modeScale m = drop (grade m) $ marks
+modeMarks :: Mode -> [Bool]
+modeMarks m = drop (grade m) $ marks
 
 markNotes :: Key -> Mode -> [(Note, Bool)]
-markNotes k m = zip (chromatic k) (modeScale m)
+markNotes k m = zip (chromatic k) (modeMarks m)
 
 markIntervals :: Mode -> [(Interval, Bool)]
-markIntervals m = zip intervals (modeScale m)
+markIntervals m = zip intervals (modeMarks m)
 
 markString :: Key -> Mode -> Note -> [Bool]
 markString k m n = map snd . dropWhile ((/=n) . fst) $ markNotes k m
@@ -54,8 +50,8 @@ allStrings k m = map (markString k m) guitarStrings
 getMarked :: [(a, Bool)] -> [a]
 getMarked = map fst . filter snd . take (length [C ..])
 
-scale :: Key -> Mode -> [Note]
-scale k m = getMarked $ markNotes k m
+modeScale :: Key -> Mode -> [Note]
+modeScale k m = getMarked $ markNotes k m
 
 modeIntervals :: Mode -> [Interval]
 modeIntervals m = getMarked $ markIntervals m
@@ -73,9 +69,9 @@ neckHeader = unwords . take neckLength . cycle . map fret $ [0..11]
 printEverything :: Key -> Mode -> IO ()
 printEverything k m = do
     putStrLn $ show k ++ " " ++ show m
-    print $ scale k m
+    print $ modeScale k m
     print $ modeIntervals m
     putStrLn $ neckHeader
     putStr $ unlines $ map printableString (allStrings k m)
 
-main = printEverything A Aeolian
+main = printEverything A Ionian
