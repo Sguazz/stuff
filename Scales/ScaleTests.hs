2eda85519e679fcea4343854ba689060d71b76b4
Sguazz <p.sguazz@gmail.com>
2015-06-30 18:02:05 +0300

    Moar tests
    Well, just one, really.

--- Scales/ScaleTests.hs
+++ Scales/ScaleTests.hs
@@ -25,6 +25,8 @@ instance Arbitrary Note where
 -- Actual tests --
 ------------------
 
+-- Scales and stuff
+
 prop_modulate :: Scale -> Mode -> Key -> Mode -> Bool
 prop_modulate s m1 k1 m2 = sortedScale s m2 k1 == sortedScale s m1 k2
   where k2 = findModulation m1 k1 m2
@@ -33,6 +35,12 @@ prop_transpose :: Scale -> Mode -> Key -> Bool
 prop_transpose s m k = all (== sortedScale s m k) scales
   where scales = map (sortedScale' s) . tops $ relativeModes m k
 
+-- Layout and stuff
+
+prop_padding :: [String] -> Bool
+prop_padding as = all ((== l) . length) $ padList as
+  where l = maximum . map length $ as
+
 -------------
 -- Helpers --
 -------------
@@ -40,6 +48,7 @@ prop_transpose s m k = all (== sortedScale s m k) scales
 sortedScale :: Scale -> Mode -> Key -> [Note]
 sortedScale s m k = sort . tops $ scaleNotes s m k
 
+sortedScale' :: Scale -> (Key, Mode) -> [Note]
 sortedScale' s (k, m) = sortedScale s m k
 
 -----------------------
